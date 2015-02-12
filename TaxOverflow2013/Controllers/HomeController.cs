0cdc72294860d45d910494abadd1eae040120ce6
Caroline Chonko <cchonko@gru.edu>
2015-02-12 16:01:08 -0500

    Minification, Logo Design Started

--- TaxOverflow2013/Controllers/HomeController.cs
+++ TaxOverflow2013/Controllers/HomeController.cs
@@ -13,20 +13,6 @@ namespace TaxOverflow2013.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
-
         public ActionResult PostQuestion()
         {
             ViewBag.Message = "Post a question";
