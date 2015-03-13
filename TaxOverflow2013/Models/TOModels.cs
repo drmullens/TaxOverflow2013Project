64335e4011d79f2cc024006ab4a5de3f1d82032d
David.Mullens <DavidRMullens78@gmail>
2015-03-13 18:17:37 -0400

    adding line for update

--- TaxOverflow2013/Models/TOModels.cs
+++ TaxOverflow2013/Models/TOModels.cs
@@ -8,5 +8,6 @@ namespace TaxOverflow2013.Models
     public class CreateModels
     {
         //This class will use an entity class to fetch data for use in the corresponding views
+        //just adding a line for an update
     }
 }
