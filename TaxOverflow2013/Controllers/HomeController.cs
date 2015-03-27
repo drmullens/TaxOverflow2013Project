70127f6498186834143925f1b361fca55d7fb453
David.Mullens <DavidRMullens78@gmail>
2015-03-27 18:14:26 -0400

    started working on the controllers

--- TaxOverflow2013/Controllers/HomeController.cs
+++ TaxOverflow2013/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web.Mvc;
 using TaxOverflow2013.Models;
+using System.Linq;
 
 namespace TaxOverflow2013.Controllers
 {
@@ -8,14 +9,40 @@ namespace TaxOverflow2013.Controllers
     {
         public ActionResult Index()
         {
+
+            //TODO: put logic here to create new user on table if current user does not exist
+
+            string currentUser;
+
+            currentUser = User.Identity.Name;
+
+            using (var context = new TestTODBEntities())
+            {
+                var users = context.Users.Where(c => c.UserName == currentUser).ToList();
+                if (users.Count() == 0)
+                {
+                    TaxOverflow2013.Models.User newUser = new TaxOverflow2013.Models.User();
+
+                    newUser.UserName = User.Identity.Name;
+
+                    users.Add(newUser);
+                    context.SaveChanges();
+                }
+            }
             return View(new HomeModel.MockIndexModel());
         }
 
         public ActionResult Question()
         {
             ViewBag.Message = "Post a Question";
+            // TODO: load the DDL with the categories from the DB
 
-            return View();
+            using (var context = new TestTODBEntities())
+            {
+                var categories = context.Categories1.Where(c => c.CategoryID > 0).ToList();
+                return View(categories);
+            }
+
         }
 
         [HttpPost]
@@ -25,6 +52,32 @@ namespace TaxOverflow2013.Controllers
             string data = txtQuestion;
 
             //Return new QuestionID
+
+            try
+            {
+                Questions1 myQuestion = new Questions1();
+
+                myQuestion.Question = txtQuestion;
+                myQuestion.User.UserName = User.Identity.Name.ToString();
+                myQuestion.Score = 0;
+
+                int ddlValue;
+                if (Int32.TryParse(ddlCategory, out ddlValue))
+                    myQuestion.Category_CategoryID = ddlValue;
+                else
+                    //TODO: logic here to handle problem with a non-int value being passed as ddlCategory
+                    ddlValue = 0;
+                using (var context = new TestTODBEntities())
+                {
+                    context.Questions1.Add(myQuestion);
+                    context.SaveChanges();
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("{0} in post question", ex);
+            }
 
             return View("ViewQuestion", new HomeModel.MockViewQuestion(Int32.Parse("0")));
         }
