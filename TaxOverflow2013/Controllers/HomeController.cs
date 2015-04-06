6010e9e5fa88528fa48edbf2f3fc12313a38056a
David.Mullens <DavidRMullens78@gmail>
2015-04-06 12:35:02 -0400

    Home page is now working with user input answers

--- TaxOverflow2013/Controllers/HomeController.cs
+++ TaxOverflow2013/Controllers/HomeController.cs
@@ -2,51 +2,84 @@
 using System.Web.Mvc;
 using TaxOverflow2013.Models;
 using System.Linq;
+using System.Collections.Generic;
 
 namespace TaxOverflow2013.Controllers
 {
     public class HomeController : Controller
     {
-        TODBEntities context = new TODBEntities();
-        CurrentData currData = new CurrentData();
 
-        public ActionResult Index()
+
+        public ActionResult Index() 
         {
             string currentUser;
 
             currentUser = User.Identity.Name;
 
-            using (context)
+            //HomePageQuestions contains 2 lists, the top 5 most recent and top 5 highest scores without an accepted answer
+            HomeQuestionLists HomePageQuestions = new HomeQuestionLists();
+            HomePageQuestions.MostRecentQuestions = new List<QuestionList>();
+            HomePageQuestions.BestUnansweredQuestions = new List<QuestionList>();
+            
+            using (var context = new TODBEntities())
             {
                 var users = context.UserTBLs.Where(c => c.UserName == currentUser).ToList();
+
+                //add new userName to UserTBL
                 if (users.Count() == 0)
                 {
                     UserTBL newUser = new UserTBL();
 
                     newUser.UserName = User.Identity.Name;
-                    
+
                     context.UserTBLs.Add(newUser);
                     context.SaveChanges();
 
-                    users = context.UserTBLs.Where(c => c.UserName == currentUser).ToList();
                 }
-               
-                foreach (var person in users)
+
+
+                //get the top 5 most recent and top 5 highest scores without an accepted answer
+                var MostRecent = (from R in context.QuestionTBLs
+                                  orderby R.QuestionDTS descending
+                                  where !(from A in context.AnswerTBLs select A.QuestionID).Contains(R.QuestionID)
+                                  select R).Take(5).ToList();
+
+                var BestScore = (from S in context.QuestionTBLs
+                                 orderby S.Score descending
+                                 where !(from A in context.AnswerTBLs where A.Accepted == true select A.QuestionID).Contains(S.QuestionID)
+                                 select S).Take(5).ToList();
+
+
+                //fill model lists with information
+                
+                foreach(var recent in MostRecent)
                 {
-                    currData.CurrentUser = person.UserName;
-                    currData.CurrentUserID = person.UserID;
+                    QuestionList newQuestion = new QuestionList();
+                    newQuestion.aQuestion = recent;
+                    newQuestion.CategoryString = recent.CategoryTBL.Category;
+                    newQuestion.UserName = recent.UserTBL.UserName;
+                    HomePageQuestions.MostRecentQuestions.Add(newQuestion);
+
+                }
+
+                foreach(var highScores in BestScore)
+                {
+                    QuestionList newQuestion = new QuestionList();
+                    newQuestion.aQuestion = highScores;
+                    newQuestion.CategoryString = highScores.CategoryTBL.Category;
+                    newQuestion.UserName = highScores.UserTBL.UserName;
+                    HomePageQuestions.BestUnansweredQuestions.Add(newQuestion);
                 }
             }
 
             //TODO: Set up home page to get the top five by highest score and most recent
-            return View(new HomeModel.MockIndexModel());
+            return View(HomePageQuestions);
         }
 
         public ActionResult Question()
         {
             ViewBag.Message = "Post a Question";
-            // TODO: load the DDL with the categories from the DB
-
+            TODBEntities context = new TODBEntities();
             using (context)
             {
                 var categories = context.CategoryTBLs.Where(c => c.CategoryID > 0).ToList();
@@ -60,8 +93,7 @@ namespace TaxOverflow2013.Controllers
         public ActionResult Question(string txtQuestion, string ddlCategory) //[optional] string newCategory = ""
         {
             string data = txtQuestion;
-
-            //Return new QuestionID
+            QuestionStream NewQuestion = new QuestionStream();
 
             try
             {
@@ -70,7 +102,8 @@ namespace TaxOverflow2013.Controllers
                 myQuestion.Question = txtQuestion;
                 myQuestion.Score = 0;
                 myQuestion.QuestionDTS = DateTime.Now;
-                myQuestion.UserID = currData.CurrentUserID;
+                myQuestion.UserID = GetCurrentUser();
+
 
                 int ddlValue;
                 if (Int32.TryParse(ddlCategory, out ddlValue))
@@ -78,15 +111,43 @@ namespace TaxOverflow2013.Controllers
                 else
                     //TODO: logic here to handle problem with a non-int value being passed as ddlCategory
                     ddlValue = 0;
-                using (context)
+                using (var context = new TODBEntities())
                 {
-
+                    //adds new record of question to the DB
                     context.QuestionTBLs.Add(myQuestion);
-                    //context.SaveChanges();
+                    context.SaveChanges();
 
-                    var QID = context.QuestionTBLs.Max(b=> b.QuestionID);
+                    //Return new QuestionID
+                    var QID = context.QuestionTBLs.Max(b => b.QuestionID);  
                     myQuestion.QuestionID = QID;
-                    ViewData.Add("QuestionID", myQuestion.QuestionID);
+
+                    NewQuestion.MainQuestion = myQuestion;
+
+                    var QComm = context.QuestionCommentTBLs.Where(a => a.QuestionID == myQuestion.QuestionID).ToList();
+                    foreach (var comment in QComm)
+                    {
+                        QuestionCommentStream QComment = new QuestionCommentStream();
+                        QComment.QComment = comment;
+                        QComment.QCUserName = comment.UserTBL.UserName;
+                        NewQuestion.RelatedQuestionComments.Add(QComment);
+                    }
+
+                    var QA = context.AnswerTBLs.Where(b => b.QuestionID == myQuestion.QuestionID).ToList();
+                    foreach (var ans in QA)
+                    {
+                        AnswerStream anAnswer = new AnswerStream();
+                        anAnswer.MainAnswer = ans;
+                        var ansComm = context.AnswerCommentTBLs.Where(d => d.AnswerID == ans.AnswerID).ToList();
+                        foreach (var comment in ansComm)
+                        {
+                            AnswerCommentStream AComment = new AnswerCommentStream();
+                            AComment.AComment = comment;
+                            AComment.ACUserName = comment.UserTBL.UserName;
+                            anAnswer.RelatedAnswerComments.Add(AComment);
+                        }
+
+                        NewQuestion.RelatedAnswers.Add(anAnswer);
+                    }
                 }
 
             }
@@ -94,10 +155,10 @@ namespace TaxOverflow2013.Controllers
             {
                 Console.WriteLine("{0} in post question", ex);
             }
-            
+
 
             //TODO: dont send the whole db, create a model that would be the question, list of comments, answers and their comments
-            return View("ViewQuestion", new TODBEntities());
+            return View("ViewQuestion", NewQuestion);
         }
 
         public ActionResult QuestionList()
@@ -162,5 +223,43 @@ namespace TaxOverflow2013.Controllers
                 return null;
             }
         }
+
+
+        #region "Get functions"
+
+        private int GetCurrentUser()
+        {
+                using (var context = new TODBEntities())
+                {
+                    var currUser = context.UserTBLs.Where(c => c.UserName == User.Identity.Name).ToList();
+                    if (currUser.Count > 0)
+                    {
+                        foreach (var person in currUser)
+                        {
+                            return person.UserID;
+                        }
+                    }
+                }
+                return -1;  //no such value send -1 to show error
+        }
+
+        private string GetCategoryByID(int catID)
+        {
+            using (var context = new TODBEntities())
+            {
+                var catString = context.CategoryTBLs.Where(c => c.CategoryID == catID).ToList();
+                if (catString.Count > 0 )
+                {
+                    foreach(var catItem in catString)
+                    {
+                        return catItem.Category;
+                    }
+                }
+            }
+
+            return "";  //No such value, redirect to error screen
+        }
+
+        #endregion
     }
 }
---
+++ TaxOverflow2013/Models/QuestionList.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TaxOverflow2013.Models
+{
+    public class QuestionList
+    {
+
+        public QuestionTBL aQuestion { get; set; }
+
+        public string CategoryString { get; set; }
+
+        public string UserName { get; set; }
+    }
+
+    //this model is used on the home page for the top 5 most recent and top 5 unanswered
+    public class HomeQuestionLists
+    {
+        public List<QuestionList> MostRecentQuestions { get; set; }
+
+        public List<QuestionList> BestUnansweredQuestions { get; set; }
+    }
+}
